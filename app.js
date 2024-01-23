8a9cb29c7d3a3d6fdc2ed995af7b7c0c354f3eb5
Paola Cernada <96987381+paolacernada@users.noreply.github.com>
2024-01-23 17:55:31 -0500

    all requirements, but timestamp present and working.

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`
+    <h1>Welcome to My Express App</h1>
+    <p>Use the following links to navigate to different routes:</p>
+    <ul>
+      <li><a href="/api/dates/today">Today's Date</a></li>
+      <li><a href="/api/dates/tomorrow">Tomorrow's Date</a></li>
+      <li><a href="/api/dates/yesterday">Yesterday's Date</a></li>
+      <li><a href="/api/day-of-week/2024/1/23">Day of the Week for a Specific Date</a></li>
+      <li><a href="/api/current-time">Current Time</a></li>
+      <li><a href="/api/current-time?format=12">Current Time in 12-hour Format</a></li>
+      <li><a href="/api/timestamp">Current Timestamp in Milliseconds</a></li>
+    </ul>
+  `);
 });
 
 
@@ -17,7 +29,6 @@ app.get('/api/dates/today', (req, res) => {
 
     const currentDate = dayjs().format("dddd MMM DD, YYYY");
   
-    // Send the text in a JSON object
     res.json({ "Current date": currentDate });
   });
 
@@ -27,7 +38,6 @@ app.get('/api/dates/tomorrow', (req, res) => {
 
     const dateTomorrow = dayjs().add(1, "day").format("dddd MMM DD, YYYY");
   
-    // Send the text in a JSON object
     res.json({ "Tomorrow's date": dateTomorrow });
   });
 
@@ -37,7 +47,6 @@ app.get('/api/dates/yesterday', (req, res) => {
 
     const dateYesterday = dayjs().subtract(1, "day").format("dddd MMM DD, YYYY");
   
-    // Send the text in a JSON object
     res.json({ "Yesterday's date": dateYesterday });
   });
 
@@ -58,15 +67,50 @@ app.get('/api/day-of-week/:year/:month/:day', (req, res) => {
 
     const dayOfTheWeek = date.format("dddd");
   
-    // Send the text in a JSON object
     res.json({ "day of the week": dayOfTheWeek });
   });
+
+
+// Returns the current time in the format 19:20:30. This endpoint accepts a query parameter 
+// called format that can be used to change the format of the time. 
+// For example, /api/current-time?format=12 should return the time in the format 7:20:30 PM. 
+// The default format should be 24.
+app.get('/api/current-time', (req, res) => {
+
+  const timeFormat = req.query.format;
+
+  if (timeFormat === "12") {
+
+    const currentTime = dayjs().format("h:mm:ss A");
+
+    res.json({ "The current time is": currentTime });
+
+  } else if (timeFormat === "24" || timeFormat === undefined) {
+
+    const currentTime = dayjs().format("H:mm:ss");
+
+    res.json({ "The current time is": currentTime });
+  } else {
+
+    res.json({ "error": "Please enter format to be 12 hour or 24 hour clock " });
+  }
+});
+
+// Returns the current timestamp in milliseconds. 
+// This endpoint should accept a query parameter called format that can be used to change the format 
+// of the timestamp. For example, /api/timestamp?format=seconds returns the timestamp in seconds. 
+// The default format should be milliseconds.
+app.get('/api/timestamp', (req, res) => {
+
+  //Still need to work on this.
+
+});
 
 
 // Returns an error message if the route is not found
 app.get('*', function (req, res) {
     res.status(404).json({ error: 'Not found' });
-  })
+  });
 
 
 // start server
